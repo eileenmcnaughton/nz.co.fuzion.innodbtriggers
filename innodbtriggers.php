42cdd194fb3ffb00a34950819e86da8cbaace8f7
Seamus Lee <seamuslee001@gmail.com>
2022-12-05 15:06:32 +1100

    Upgrade civix generated code to latest version

--- innodbtriggers.php
+++ innodbtriggers.php
@@ -12,17 +12,6 @@ function innodbtriggers_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function innodbtriggers_civicrm_xmlMenu(&$files) {
-  _innodbtriggers_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -75,56 +64,6 @@ function innodbtriggers_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function innodbtriggers_civicrm_managed(&$entities) {
-  _innodbtriggers_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function innodbtriggers_civicrm_caseTypes(&$caseTypes) {
-  _innodbtriggers_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function innodbtriggers_civicrm_angularModules(&$angularModules) {
-_innodbtriggers_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function innodbtriggers_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _innodbtriggers_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_alterLogTables().
  *
  * @param array $logTableSpec
@@ -158,4 +97,22 @@ function innodbtriggers_civicrm_alterLogTables(&$logTableSpec) {
     }
     $logTableSpec[$tableName]['indexes'] = array_merge($indexArray, $contactIndexes);
   }
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function innodbtriggers_civicrm_postInstall() {
+  _innodbtriggers_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function innodbtriggers_civicrm_entityTypes(&$entityTypes) {
+  _innodbtriggers_civix_civicrm_entityTypes($entityTypes);
 }
